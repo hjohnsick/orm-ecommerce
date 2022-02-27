e61f808cb54bae90a89ec0a011ed6f5e3e0a6d4d
hjohnsick <hjohnsick@yahoo.com>
2022-02-27 06:43:51 -0500

    added column definitions for Category

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Table column definitions go here
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
